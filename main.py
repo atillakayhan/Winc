7f61a700edda64b5645bdd105de79872da353608
atillakayhan <110838807+atillakayhan@users.noreply.github.com>
2022-12-02 16:15:53 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,38 +1,33 @@
 # Do not modify these lines
-__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
-__human_name__ = 'strings'
+__winc_id__ = '7b9401ad7f544be2a23321292dd61cb6'
+__human_name__ = 'arguments'
 
 # Add your code after this line
 
-#part 1
-scorer_0 = "Ruud Gullit " #First player to score a goal 
-scorer_1 = "Marco van Basten " #Second player to score a goal 
-goal_0 = 32 #minute of the first goal
-goal_1 = 54 #minute of the second goal
+#Part 1
+def greet(name, template='Hello, <name>!'):
+    new_template = template.replace('<name>', name)
+    return new_template
 
-scorers = f"{scorer_0}{goal_0}, {scorer_1}{goal_1}"
-print (scorers)
-report = f"{scorer_0}scored in the {goal_0}nd minute\n{scorer_1}scored in the {goal_1}th minute" 
-print (report)
+print(greet('atilla', 'yooow <name>'))
 
-#part 2
-player = "Paul Walker"
-print (player.find(" ")) #geeft aan 6
-first_name = player[0:(player.find(" "))] #van begin tot spatie is voornaam
-print (first_name)
-last_name = player[(player.find(" ")+ 1):(len(player))] #vanaf de spatie plus 1 zodat je de eerste letter van achternaam pakt en dan tot het eind van de hele naam
-print (last_name)
-last_name_len = len(player[(player.find(" ")+ 1):(len(player))]) 
-print (last_name_len)
+#Part 2
+dict_info_planets = {
+        'sun': 274, 'jupiter': 24.9, 'neptune': 11.2, 'saturn': 10.4,
+        'earth': 9.8, 'uranus': 8.9, 'venus': 8.9, 'mars': 3.7, 
+        'mercury': 3.7, 'moon': 1.6, 'pluto': 0.6
+    }
+def force(mass, body='earth'):
+    world = (dict_info_planets[body])
+    calculation = (mass * world)
+    return round(calculation)
+print(force(100))
 
-name_short = f"{first_name[0]}. {last_name}"
-print (name_short)
-
-chant = ((first_name) + '! ') * (len(first_name)- 1) + first_name + "!"
-print (chant) 
-print (len(chant)) #hiermee kijken hoelang de zin is en dus wat de laatste letter/getal is
-good_chant = (chant[len(chant) - 1] != " ") #-1 omdat het vanaf 0 begint met tellen
-print (good_chant) #geeft aan true dus laatste letter van chant is geen spatie
-
-
-
+#part 3
+def pull(m1, m2, d):
+    gravity = 6.674*10**-11
+    distance_2 = d ** 2
+    calculation_1 = (m1 *m2) / distance_2
+    calculation_2 = gravity * calculation_1
+    return calculation_2
+print(pull(800, 1500, 3))
